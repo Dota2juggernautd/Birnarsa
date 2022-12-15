bff8037cc84cfa727bb095b2be56ac11728ffa00
Juggernautd <93264085+Dota2juggernautd@users.noreply.github.com>
2022-12-15 11:18:44 +0500

    Added

---
+++ app/src/main/java/com/example/ravonyoltest/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.ravonyoltest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int pic_id = 123;
+    // Define the button and imageview type variable
+    Button camera_open_id;
+    ImageView click_image_id;
+
+    Button BSelectImage, BsendImage;
+    int SELECT_PICTURE = 200;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // By ID we can get each component which id is assigned in XML file get Buttons and imageview.
+        BsendImage = findViewById(R.id.sendImage);
+        camera_open_id = findViewById(R.id.camera_button);
+        click_image_id = findViewById(R.id.click_image);
+        BSelectImage = findViewById(R.id.BSelectImage);
+
+        // Camera_open button is for open the camera and add the setOnClickListener in this button
+        camera_open_id.setOnClickListener(v -> {
+            // Create the camera_intent ACTION_IMAGE_CAPTURE it will open the camera for capture the image
+            Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            // Start the activity with camera_intent, and request pic id
+            startActivityForResult(camera_intent, pic_id);
+
+
+        });
+        BSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageChooser();
+            }
+        });
+        sendMessage();
+
+    }
+
+    //    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        // Match the request 'pic id with requestCode
+//        if (requestCode == pic_id) {
+//            // BitMap is data structure of image file which store the image in memory
+//            Bitmap photo = (Bitmap) data.getExtras().get("data");
+//            // Set the image in imageview for display
+//            click_image_id.setImageBitmap(photo);
+//        }
+//    }
+    void imageChooser() {
+
+        // create an instance of the
+        // intent of the type image
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == pic_id) {
+            // BitMap is data structure of image file which store the image in memory
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            // Set the image in imageview for display
+            click_image_id.setImageBitmap(photo);
+        }
+        if (resultCode == RESULT_OK) {
+
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url of the image from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // update the preview image in the layout
+                    click_image_id.setImageURI(selectedImageUri);
+                }
+            }
+        }
+    }
+
+    public void sendMessage() {
+        if (null != click_image_id.getDrawable()) {
+            //imageview has image
+            camera_open_id.setVisibility(View.GONE);
+            BSelectImage.setVisibility(View.GONE);
+            BsendImage.setVisibility(View.VISIBLE);
+
+        } else {
+            //imageview has no image
+
+        }
+
+    }
+}
